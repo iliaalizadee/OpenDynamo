98a8896ef5293d28d42318d242f23ba7abe094ec
Ilia <iliaalizadee@gmail.com>
2025-11-09 20:08:02 +0330

    Update mesh code

--- mesh.cpp
+++ mesh.cpp
@@ -7,16 +7,40 @@ Mesh::Mesh::Mesh(const std::string &mesh_file)
     if (cg_open(mesh_file.c_str(), CG_MODE_READ, &file_index))
         std::cerr << cg_get_error() << std::endl;
 
-    cg_zone_read(file_index, base_index, zone_index, "ZONE", mesh_size[0]);
+    base_index = 1;
+    zone_index = 1;
 
-    irmin = 1;
-    irmax[0] = mesh_size[0][0];
-    irmax[1] = mesh_size[0][1];
-    irmax[2] = mesh_size[0][2];
+    cg_zone_read(file_index, base_index, zone_index, zone_name.data(), mesh_size[0]);
 
-    cg_coord_read(file_index, base_index, zone_index, "COORD_X", CGNS_ENUMV(RealDouble), &irmin, &irmax[0], coord_x.data());
-    cg_coord_read(file_index, base_index, zone_index, "COORD_Y", CGNS_ENUMV(RealDouble), &irmin, &irmax[1], coord_y.data());
-    cg_coord_read(file_index, base_index, zone_index, "COORD_Z", CGNS_ENUMV(RealDouble), &irmin, &irmax[2], coord_z.data());
+    mesh_first_index = 1;
+    mesh_last_index[0] = mesh_size[0][0];
+    mesh_last_index[1] = mesh_size[0][1];
+    mesh_last_index[2] = mesh_size[0][2];
+
+    cg_coord_read(file_index, base_index, zone_index, "COORD_X", CGNS_ENUMV(RealDouble), &mesh_first_index, &mesh_last_index[0], coordinate_x.data());
+    cg_coord_read(file_index, base_index, zone_index, "COORD_Y", CGNS_ENUMV(RealDouble), &mesh_first_index, &mesh_last_index[1], coordinate_y.data());
+    cg_coord_read(file_index, base_index, zone_index, "COORD_Z", CGNS_ENUMV(RealDouble), &mesh_first_index, &mesh_last_index[2], coordinate_z.data());
+
+    cg_nbocos(file_index, base_index, zone_index, &number_of_boundaries);
+
+    for(int boundary_index = 0; boundary_index < number_of_boundaries; boundary_index++)
+    {
+        Boundary boundary;
+        cg_boco_info(file_index, 
+            base_index, 
+            zone_index, 
+            boundary_index, 
+            boundary.name.data(), 
+            &boundary.type, 
+            &boundary.pointset_type,
+            &boundary.number_of_points,
+            &boundary.normal_index,
+            &boundary.normal_list_size,
+            &boundary.normal_data_type,
+            boundary.dataset.data()
+        );
+        
+    }
 
     cg_close(file_index);
 }
--- mesh.hpp
+++ mesh.hpp
@@ -1,23 +1,45 @@
 #ifndef MESH_H
 #define MESH_H
+
+
 #include <cgnslib.h>
 #include <string>
 #include <vector>
 
 namespace Mesh
 {
+    typedef struct Boundary
+    {
+        std::string name;
+        CG_BCType_t type;
+        CG_PointSetType_t pointset_type;
+        cgsize_t number_of_points;
+        int normal_index;
+        cgsize_t normal_list_size;
+        CG_DataType_t normal_data_type;
+        std::vector<int> dataset;
+    } Boundary;
+
+
     class Mesh
     {
     private:
         int file_index, base_index, zone_index;
-        std::vector<std::vector<std::vector<double>>> coord_x, coord_y, coord_z;
+        std::string zone_name;
+
+        std::vector<std::vector<std::vector<double>>> coordinate_x, coordinate_y, coordinate_z;
+        std::vector<Boundary> boundaries;
+
         cgsize_t mesh_size[3][3];
-        cgsize_t irmin;
-        cgsize_t irmax[3];
+        cgsize_t mesh_first_index;
+        cgsize_t mesh_last_index[3];
+        int number_of_boundaries;
 
     public:
         Mesh(const std::string &mesh_file);
         ~Mesh();
-    }; 
+    };
 }
+
+
 #endif
